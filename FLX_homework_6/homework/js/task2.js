f4b9f530889027f9934d10fad0dea21a15c4ef20
Yaryna Kharaton <kharaton.yaryna@gmail.com>
2019-02-05 22:44:18 +0200

    Update task2.js

--- FLX_homework_6/homework/js/task2.js
+++ FLX_homework_6/homework/js/task2.js
@@ -7,15 +7,15 @@ if (amount < 9999999 && amount > 0 && discount < 99 && discount > 0) {
 	let savedMoney = (amount * discount) / 100;
 	let priceWithDiscount = amount - savedMoney;
 
-	savedMoney = savedMoney.toFixed(2);
-	priceWithDiscount = priceWithDiscount.toFixed(2);
+	savedMoney = +savedMoney.toFixed(2);
+	priceWithDiscount = +priceWithDiscount.toFixed(2);
 
 	alert(`
-		Price without discount: ${amount} \n
-		Discount: ${discount}% \n
-		Price with discount: ${priceWithDiscount} \n
-		Saved money: ${savedMoney} \n`
-  );
+	Price without discount: ${amount} \n
+	Discount: ${discount}% \n
+	Price with discount: ${priceWithDiscount} \n
+	Saved money: ${savedMoney} \n`
+	);
 } else {
   alert('Invalid input data');
 }
